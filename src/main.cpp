46a92628ff408cfd61575dc472b7bda5625864b0
AnnelotJ <janssenannelot@gmail.com>
2022-05-17 15:45:35 +0200

    dafdaaf

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
+/*  Student: annelot janssen
+    Studentnummer: 0987583
+    Vakcode: TINBES-02
+*/
+
 #include <Arduino.h>
+#include <EEPROM.h>
+
+#define buffsize 12
+
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600); 
+    
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
+void commmandLine(){ 
+
+}
+
+void retrieve () {
+
+}
+
+fileName
 
